ded147a811198d73972053f725709944e5ddd662
RubenBeeftink <rubenbeeftink@hotmail.nl>
2024-09-23 14:18:05 +0200

    feature: add laravel fortify for api authentication

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthenticatedSessionController extends Controller
+{
+    public function store(LoginRequest $request): JsonResponse
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['error' => 'No matching credentials.']);
+        }
+
+        Auth::login($user);
+
+        $token = auth()->user()->createToken('token');
+
+        return response()->json([
+            'data' => [
+                'api_token' => $token->plainTextToken,
+            ]
+        ]);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): Response
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->noContent();
+    }
+}
---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\Validation\ValidationException;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * @throws ValidationException
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $request->validate([
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->string('password')),
+            'email_verified_at' => Carbon::now(),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        $token = auth()->user()->createToken('auth');
+
+        return response()->json([
+            'data' => [
+                'api_token' => $token->plainTextToken,
+            ]
+        ]);
+    }
+}
